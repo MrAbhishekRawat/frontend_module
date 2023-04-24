dd1fd84a0c853108ae925463c7681760025f40cf
MrAbhishekRawat <abhishekr.rawat8@gmail.com>
2023-04-24 13:28:48 +0530

    letget task9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./Components/ExpenseItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const expenses = [
+
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      locationOfExpenditure: 'Domestic'
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12), locationOfExpenditure: 'Electronic' },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      locationOfExpenditure: 'Documents'
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      locationOfExpenditure: 'Furniture'
+    },
+
+  ];
+  return(
+    <div>
+      <h2>Expense Item</h2>
+      <ExpenseItem 
+      title={expenses[0].title}
+      amount={expenses[0].amount}
+      date={expenses[0].date}
+      category={expenses[0].locationOfExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem
+      title={expenses[1].title}
+      amount={expenses[1].amount}
+      date={expenses[1].date}
+      category={expenses[1].locationOfExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem
+      title={expenses[2].title}
+      amount={expenses[2].amount}
+      date={expenses[2].date}
+      category={expenses[2].locationOfExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem
+      title={expenses[3].title}
+      amount={expenses[3].amount}
+      date={expenses[3].date}
+      category={expenses[3].locationOfExpenditure}
+      ></ExpenseItem>
     </div>
   );
 }
-
 export default App;
---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
+    return (
+      <div className='expense-item'>
+        <div>{props.date.toISOString()}</div>
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+        <div className='locationofexpenditure'>{props.category}</div>
+      </div>
+      
+    );
+  }
+  
+export default ExpenseItem;
