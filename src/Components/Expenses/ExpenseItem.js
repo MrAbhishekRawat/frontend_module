beb7f9315fd940cac75b5996f2773bc1d7b93741
MrAbhishekRawat <abhishekr.rawat8@gmail.com>
2023-04-25 14:13:33 +0530

    adva react task2

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const [title, setTitle]= useState(props.title)
+
   const clickHandler= ()=>{
-    console.log("clicked!!")
+    setTitle('Updated');
+    console.log(title);
   }
+
+  const [amount, setAmount]= useState(props.amount)
+
+  const clickHandler2= ()=>{
+    setAmount(100);
+    console.log(amount);
+  }
+
   const delHandler=()=>{
     props.onDelete(props.id);
   }
@@ -15,11 +26,12 @@ const ExpenseItem = (props) => {
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${amount}</div>
       </div>
       <button onClick= {clickHandler}>Change Title</button>
       <button onClick= {delHandler}>Del Expense</button>
+      <button onClick= {clickHandler2}>Change Amount</button>
     </Card>
   );
 }
